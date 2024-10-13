4ff664f270eda220b8d40ead26a592b5e3600a16
lam <lambro2510@gmail.com>
2024-10-14 02:30:50 +0700

    update

---
+++ src/main/java/com/lamld/supportmanualtest/server/configs/MetricsConfiguration.java
@@ -0,0 +1,12 @@
+package com.lamld.supportmanualtest.server.configs;
+
+import io.micrometer.core.instrument.binder.jvm.JvmHeapPressureMetrics;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfiguration {
+
+
+}
